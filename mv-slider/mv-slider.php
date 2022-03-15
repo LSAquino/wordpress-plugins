678b55b27090505fa8745bf5bf2c68d25b5cb481
Lucas Aquino <lucas.silva22@live.com>
2022-03-15 11:20:01 -0300

    Método active, deactive e uninstall

--- mv-slider/mv-slider.php
+++ mv-slider/mv-slider.php
@@ -14,7 +14,7 @@
  * Domain Path: /languages
  */
 
- /*
+/*
 MV Slider is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
@@ -27,25 +27,44 @@ You should have received a copy of the GNU General Public License
 along with MV Slider. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
-if( ! defined( 'ABSPATH') ){
+if (!defined('ABSPATH')) {
     exit;
 }
 
-if( ! class_exists( 'MV_Slider' ) ){
-    class MV_Slider{
-        function __construct(){
+if (!class_exists('MV_Slider')) {
+    class MV_Slider
+    {
+        function __construct()
+        {
             $this->define_constants();
         }
 
-        public function define_constants(){
-            define( 'MV_SLIDER_PATH', plugin_dir_path( __FILE__ ) );
-            define( 'MV_SLIDER_URL', plugin_dir_url( __FILE__ ) );
-            define( 'MV_SLIDER_VERSION', '1.0.0' );
+        public function define_constants()
+        {
+            define('MV_SLIDER_PATH', plugin_dir_path(__FILE__));
+            define('MV_SLIDER_URL', plugin_dir_url(__FILE__));
+            define('MV_SLIDER_VERSION', '1.0.0');
         }
 
+        public static function activate()
+        {
+            update_option('rewrite_rules', '');
+        }
+        
+        public static function deactivate()
+        {
+            flush_rewrite_rules();
+        }
+
+        public static function uninstall()
+        {
+        }
+    }
+
+    if (class_exists('MV_Slider')) {
+        register_activation_hook(__FILE__, array('MV_Slider', 'activate'));
+        register_deactivation_hook(__FILE__, array('MV_Slider', 'deactivate'));
+        register_uninstall_hook(__FILE__, array('MV_Slider', 'uninstall'));
+        $mv_slider = new MV_Slider();
     }
 }
-
-if( class_exists( 'MV_Slider' ) ){
-    $mv_slider = new MV_Slider();
-} 
