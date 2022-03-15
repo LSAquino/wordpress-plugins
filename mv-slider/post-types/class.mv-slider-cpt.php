5b32b88a63a934a9ad66e203b96481607203352d
Lucas Aquino <lucas.silva22@live.com>
2022-03-15 13:49:14 -0300

    adicionando metaboxes

--- mv-slider/post-types/class.mv-slider-cpt.php
+++ mv-slider/post-types/class.mv-slider-cpt.php
@@ -1,12 +1,16 @@
-<?php 
+<?php
 
-if( !class_exists( 'MV_Slider_Post_Type') ){
-    class MV_Slider_Post_Type{
-        function __construct(){
-            add_action( 'init', array( $this, 'create_post_type' ) );
+if (!class_exists('MV_Slider_Post_Type')) {
+    class MV_Slider_Post_Type
+    {
+        function __construct()
+        {
+            add_action('init', array($this, 'create_post_type'));
+            add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
         }
 
-        public function create_post_type(){
+        public function create_post_type()
+        {
             register_post_type(
                 'mv-slider',
                 array(
@@ -17,7 +21,7 @@ if( !class_exists( 'MV_Slider_Post_Type') ){
                         'singular_name' => 'Slider'
                     ),
                     'public'    => true,
-                    'supports'  => array( 'title', 'editor', 'thumbnail' ),
+                    'supports'  => array('title', 'editor', 'thumbnail'),
                     'hierarchical'  => false,
                     'show_ui'   => true,
                     'show_in_menu'  => true,
@@ -29,10 +33,28 @@ if( !class_exists( 'MV_Slider_Post_Type') ){
                     'exclude_from_search'   => false,
                     'publicly_queryable'    => true,
                     'show_in_rest'  => true,
-                    'menu_icon' => 'dashicons-images-alt2'
+                    'menu_icon' => 'dashicons-images-alt2',
+                    // 'register_meta_box_cb' => array($this,'add_meta_boxes')
                 )
             );
         }
 
+        public function add_meta_boxes()
+        {
+            add_meta_box(
+                'mv_slider_meta_box',
+                'Link Options',
+                array($this, 'add_inner_meta_boxes'),
+                'mv-slider',
+                'normal',
+                'high',
+                array('foo' => 'bar'),
+            );
+        }
+
+        public function add_inner_meta_boxes($post)
+        {
+
+        }
     }
 }
