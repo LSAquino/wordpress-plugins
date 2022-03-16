87147e59245c2ddff5e71dba358dc7f03a13100a
Lucas Aquino <lucas.silva22@live.com>
2022-03-16 11:16:47 -0300

    Nonces e outras validações

--- mv-slider/mv-slider.php
+++ mv-slider/mv-slider.php
@@ -6,7 +6,7 @@
  * Description: My plugin's description
  * Version: 1.0
  * Requires at least: 5.6
- * Author: Marcelo Vieira
+ * Author: Lucas Aquino
  * Author URI: https://www.codigowp.net
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
