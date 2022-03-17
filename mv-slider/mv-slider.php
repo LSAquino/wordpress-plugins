6f43960921e8bebd929dec2126e830a5a62c70e5
Lucas Aquino <lucas.silva22@live.com>
2022-03-17 11:18:52 -0300

    Settings + API Options - Passando argumentos para os campos criados

--- mv-slider/mv-slider.php
+++ mv-slider/mv-slider.php
@@ -27,57 +27,50 @@ You should have received a copy of the GNU General Public License
 along with MV Slider. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
 
-if (!defined('ABSPATH')) {
+if( ! defined( 'ABSPATH') ){
     exit;
 }
 
-if (!class_exists('MV_Slider')) {
-    class MV_Slider
-    {
-        function __construct()
-        {
+if( ! class_exists( 'MV_Slider' ) ){
+    class MV_Slider{
+        function __construct(){
             $this->define_constants();
 
-            add_action('admin_menu', array($this, 'add_menu'));
+            add_action( 'admin_menu', array( $this, 'add_menu' ) );
 
-            require_once(MV_SLIDER_PATH . 'post-types/class.mv-slider-cpt.php');
+            require_once( MV_SLIDER_PATH . 'post-types/class.mv-slider-cpt.php' );
             $MV_Slider_Post_Type = new MV_Slider_Post_Type();
-            
-            require_once(MV_SLIDER_PATH . 'class.mv-slider-settings.php');
+
+            require_once( MV_SLIDER_PATH . 'class.mv-slider-settings.php' );
             $MV_Slider_Settings = new MV_Slider_Settings();
-
         }
 
-        public function define_constants()
-        {
-            define('MV_SLIDER_PATH', plugin_dir_path(__FILE__));
-            define('MV_SLIDER_URL', plugin_dir_url(__FILE__));
-            define('MV_SLIDER_VERSION', '1.0.0');
+        public function define_constants(){
+            define( 'MV_SLIDER_PATH', plugin_dir_path( __FILE__ ) );
+            define( 'MV_SLIDER_URL', plugin_dir_url( __FILE__ ) );
+            define( 'MV_SLIDER_VERSION', '1.0.0' );
         }
 
-        public static function activate()
-        {
-            update_option('rewrite_rules', '');
+        public static function activate(){
+            update_option( 'rewrite_rules', '' );
         }
 
-        public static function deactivate()
-        {
+        public static function deactivate(){
             flush_rewrite_rules();
-            unregister_post_type('mv-slider');
+            unregister_post_type( 'mv-slider' );
         }
 
-        public static function uninstall()
-        {
+        public static function uninstall(){
+
         }
 
-        public function add_menu()
-        {
+        public function add_menu(){
             add_menu_page(
                 'MV Slider Options',
                 'MV Slider',
                 'manage_options',
                 'mv_slider_admin',
-                array($this, 'mv_slider_settings_page'),
+                array( $this, 'mv_slider_settings_page' ),
                 'dashicons-images-alt2'
             );
 
@@ -86,32 +79,43 @@ if (!class_exists('MV_Slider')) {
                 'Manage Slides',
                 'Manage Slides',
                 'manage_options',
-                'post-new.php?post_type=mv-slider',
+                'edit.php?post_type=mv-slider',
                 null,
                 null
             );
+
             add_submenu_page(
                 'mv_slider_admin',
                 'Add New Slide',
                 'Add New Slide',
                 'manage_options',
-                'edit.php?post_type=mv-slider',
+                'post-new.php?post_type=mv-slider',
                 null,
                 null
             );
         }
 
-        public function mv_slider_settings_page()
-        {
-            require (MV_SLIDER_PATH . 'views/settings-page.php'); 
+        public function mv_slider_settings_page(){
+            if( ! current_user_can( 'manage_options' ) ){
+                return;
+            }
+
+            if( isset( $_GET['settings-updated'] ) ){
+                add_settings_error( 'mv_slider_options', 'mv_slider_message', 'Settings Saved', 'success' );
+            }
+            
+            settings_errors( 'mv_slider_options' );
+
+            require( MV_SLIDER_PATH . 'views/settings-page.php' );
         }
+
     }
 }
 
-if (class_exists('MV_Slider')) {
-    register_activation_hook(__FILE__, array('MV_Slider', 'activate'));
-    register_deactivation_hook(__FILE__, array('MV_Slider', 'deactivate'));
-    register_uninstall_hook(__FILE__, array('MV_Slider', 'uninstall'));
+if( class_exists( 'MV_Slider' ) ){
+    register_activation_hook( __FILE__, array( 'MV_Slider', 'activate' ) );
+    register_deactivation_hook( __FILE__, array( 'MV_Slider', 'deactivate' ) );
+    register_uninstall_hook( __FILE__, array( 'MV_Slider', 'uninstall' ) );
 
     $mv_slider = new MV_Slider();
-}
+} 
