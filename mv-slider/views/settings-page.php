6ee4574020da10196d5d68ff107affe258b5312a
Lucas Aquino <lucas.silva22@live.com>
2022-03-17 09:19:25 -0300

    Settings + Options API - Adicionando seções e campos pt 2

--- mv-slider/views/settings-page.php
+++ mv-slider/views/settings-page.php
@@ -1,6 +1,10 @@
 <div class="wrap">
     <h1><?= get_admin_page_title(); ?></h1>
     <form action="options.php" method="POST">
-        
+        <?php 
+        settings_fields('mv_slider_group');
+        do_settings_sections('mv_slider_page1');
+        submit_button('Save Settings');
+        ?>
     </form>
 </div>
